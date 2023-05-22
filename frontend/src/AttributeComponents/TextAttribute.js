4bd5ef0c54a0f6024e917967012afec8e4341d40
FroggyLex <128695217+FroggyLex@users.noreply.github.com>
2023-05-22 12:50:31 +0200

    Delete frontend/src/AttributeComponents directory

--- frontend/src/AttributeComponents/TextAttribute.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const TextAttribute = ({ value }) => <p className='text-attribute'>{value}</p>;
-
-export default TextAttribute;
-
--- frontend/src/AttributeComponents/TitleAttribute.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TextAttribute = ({ value }) => <h3 className="title-attribute">{value}</h3>;
-
-export default TextAttribute;
